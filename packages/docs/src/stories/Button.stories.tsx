3d12923dad91e4906a734a6bad50ff67f678ffb0
kassio oliveira <kassiooliveira875@gmail.com>
2023-05-19 10:50:01 -0300

    token documentation

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -3,7 +3,7 @@ import {Button,ButtonProps} from '@kdesign-ui/react'
 
 
 export default {
-    title:'Button',
+    title:'Form/Button',
     component:Button,
     args:{
         children:'Enviar'
